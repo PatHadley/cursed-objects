1aba8810e7a72ad6fdde96f496c72a47b10b437f
Pat Hadley <pat@pathadley.net>
2019-02-06 17:48:51 +0000

    Basic PAS prodder working

---
+++ pas-tester.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+const request = require('request');
+// const argv = require('yargs').argv;
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
+
+let itemId = getRandomInt(999999);
+let url = `https://finds.org.uk/database/artefacts/record/id/${itemId}/format/json`;
+
+console.log(itemId);
+
+request(url, function (err, response, body) {
+	if(err){
+		console.log('error', error);
+	} else {
+		let metadata = JSON.parse(body);
+		let description = metadata.record[0].description;
+		console.log(description);
+	}
+});
