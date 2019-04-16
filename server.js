d85642a87d121cdc990940a1e12cd7d6445a9783
Neil <neilrhawkins@gmail.com>
2019-04-16 22:35:20 +0100

    Fiddle with some bits

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
 const app = express();
 
 app.use(express.static('public'));
