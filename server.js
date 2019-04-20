77a4ae5121a2e62ccc9351259583ff26a75a1fdd
Neil <neilrhawkins@gmail.com>
2019-04-20 10:39:13 +0100

    Dates and images

--- server.js
+++ server.js
@@ -23,15 +23,18 @@ app.post('/', function(req, res) {
 			res.render('index', { story: null, error: 'Something went wrong... no record defined for that ID.'  });
 		} 
 		else {
+			const startDate = record.numdate1 < 0 ? `${record.numdate1.substring(1)} BC` : `${record.numdate1} AD`;
+			const endDate = record.numdate2 < 0 ? `${record.numdate2.substring(1)} BC` : `${record.numdate2} AD`;
+			const imgUrl = `https://finds.org.uk/${record.imagedir}${record.filename}`;
 			const text = 'This'
 			+ ' ' + record.primaryMaterial
 			+ ' ' + record.objecttype
-			+ ' is from between ' + record.numdate1
-			+ ' and ' + record.numdate2
+			+ ' is from between ' + startDate
+			+ ' and ' + endDate
 			+ '. It was discovered in ' + record.district
 			+ ' and reported to the Portable Antiquities Scheme and then ' + record.subsequentActionTerm
 			+ '. This is the normal story of an object being responsibly discovered and recorded…';
-			res.render('index', { story: text, error: null });
+			res.render('index', { story: text, imgUrl: imgUrl, error: null });
 		}
 	});
 });
