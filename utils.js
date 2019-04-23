e8980510e95cd5394f1ce8b330ea0414e874035b
Pat Hadley <pat@pathadley.net>
2019-04-23 16:48:45 +0100

    linting

--- utils.js
+++ utils.js
@@ -1,14 +1,14 @@
 const request = require('request');
 
 exports.getArtefact = function (itemId, callback) {
-  let url = `https://finds.org.uk/database/artefacts/record/id/${itemId}/format/json`;
-  request(url, function (err, response, body) {
-    if (err) {
-      return callback(err);
-    }
-    // console.log(body);
-    let metadata = JSON.parse(body);
-    let record = metadata.record[0];
-    callback(null, record);
-  });
-}
+	let url = `https://finds.org.uk/database/artefacts/record/id/${itemId}/format/json`;
+	request(url, function (err, response, body) {
+		if (err) {
+			return callback(err);
+		}
+		// console.log(body);
+		let metadata = JSON.parse(body);
+		let record = metadata.record[0];
+		callback(null, record);
+	});
+};
